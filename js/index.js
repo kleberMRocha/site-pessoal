44ea88f31aad22c366af362f0ab844c7e17ff88b
kleber Rocha <kleberrocha2701@gmail.com>
2020-07-15 09:30:27 -0300

    projetos

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 
     const header = document.querySelector('[data-header]');
     const progressBars = document.querySelectorAll('[data-progress]');
+    const top = document.querySelector('[data-top]');
 
     /*
     //header Parallax
@@ -14,12 +15,15 @@
     window.addEventListener('scroll',()=>{
         if(scroll){
             scroll = false;
+            
             let preogressBarL = Math.floor(progressBars[0].getBoundingClientRect().top);
                 if(window.innerHeight > preogressBarL){
+                    top.style = '';
                     progressBars.forEach(bar => {
                         bar.style = `${bar.dataset.progress} animation-duration: 3s;`
                     });
                 }else{
+                    top.style = 'display:none';
                     progressBars.forEach(bar => {
                         bar.style = ``;
                     });
